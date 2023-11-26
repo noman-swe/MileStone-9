88277e4d791495a30629acdcb83b89c47c746b24
noman-swe <noman.swe130@gmail.com>
2023-11-27 01:17:10 +0600

    51-6 Responsive pricing card with Tailwind and Hero icons

--- Module-51/react-with-tailwind/src/components/Pricing/Pricing.js
+++ Module-51/react-with-tailwind/src/components/Pricing/Pricing.js
@@ -3,9 +3,29 @@ import PricingOption from '../PricingOption/PricingOption';
 
 const Pricing = () => {
     const pricingOptions = [
-        { id: 1, name: 'Free', price: 0 },
-        { id: 2, name: 'Regular', price: 9.99 },
-        { id: 3, name: 'Premium', price: 19.99 },
+        {
+            id: 1, name: 'Free', price: 0, benefits: [
+                'lifetime free',
+                'unlimited deals',
+                'localize deals',
+                'fantastic deals',
+                'crazy deals'
+            ]
+        },
+        { id: 2, name: 'Regular', price: 9.99, benefits: [
+            'everything on free',
+            'unlimited deals',
+            'localize deals',
+            'fantastic deals',
+            'crazy deals'
+        ] },
+        { id: 3, name: 'Premium', price: 19.99, benefits: [
+            'Everything on rgular',
+            'unlimited deals',
+            'localize deals',
+            'fantastic deals',
+            'crazy deals'
+        ] },
     ]
     return (
         <div className='bg-indigo-200 mt-8 text-center p-4'>
--- Module-51/react-with-tailwind/src/components/PricingOption/PricingOption.js
+++ Module-51/react-with-tailwind/src/components/PricingOption/PricingOption.js
@@ -1,7 +1,9 @@
+import { ArrowRightIcon } from '@heroicons/react/solid';
 import React from 'react';
+import Benefit from '../Benefit/Benefit';
 
 const PricingOption = (props) => {
-    const {name, price} = props.option;
+    const { name, price, benefits } = props.option;
     return (
         <div className='bg-white p-4 rounded-lg'>
             <h2 className='bg-indigo-300 py-4 rounded-lg text-xl font-bold'>{name}</h2>
@@ -9,6 +11,25 @@ const PricingOption = (props) => {
                 <span className='text-5xl font-bold'>${price}</span>
                 <span className='text-xl font-bold text-gray-500'>/month</span>
             </p>
+
+            <div>
+                <h3 className='text-2xl text-left'>Benefits:</h3>
+                {
+                    // console.log(benefits)
+                    benefits.map(benefit => <Benefit
+                    // key={benifit.id}
+                    benefit={benefit}
+                    ></Benefit>)
+                }
+
+                <button className='bg-green-500 flex justify-center w-full mt-4 py-2 rounded-lg
+                  text-white hover:text-green-700 font-bold items-center'>Buy Now <ArrowRightIcon className='w-8 h-8  ml-2 '></ArrowRightIcon></button>
+
+                {/* <p className='flex items-center'> 
+                    <CheckCircleIcon className='w-4 h-4 text-green-500 mr-2'></CheckCircleIcon>
+                    Awesome features
+                </p> */}
+            </div>
         </div>
     );
 };
