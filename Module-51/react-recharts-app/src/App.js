6cc29d0bdf1b519c9530488d5fe5ac7205c8ffb2
noman-swe <noman.swe130@gmail.com>
2023-11-28 01:43:15 +0600

    51-9 Module Summary and react spring animation

--- Module-51/react-recharts-app/src/App.js
+++ Module-51/react-recharts-app/src/App.js
@@ -1,11 +1,23 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 import MyLineChart from './components/MyLineChart/MyLineChart';
 import SpecialChart from './components/SepecialChart/SpecialChart';
+import { useSpring, animated } from 'react-spring';
 
 function App() {
+ const [flip, set] = useState(false);
+
+  const props = useSpring({
+    to: { opacity: 1 },
+    from: { opacity: 0 },
+    reset: true,
+    reverse: flip,
+    delay: 200,
+    onRest: () => set(!flip),
+  });
   return (
     <div className="App">
+      <animated.div style={props} className={'text-6xl my-8 font-bold'}>I will fade in</animated.div>
       <MyLineChart></MyLineChart>
       <SpecialChart></SpecialChart>
     </div>
