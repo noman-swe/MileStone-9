d741e0901bf03d2b5723d9284d5ba2cb68833c31
noman-swe <noman.swe130@gmail.com>
2023-11-29 12:16:36 +0600

    52-2 Create Simple Route and simple router setup

--- Module-52/react-router/src/App.js
+++ Module-52/react-router/src/App.js
@@ -1,14 +1,20 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home/Home';
-import Friends from './components/Friends/Friends';
 import About from './components/About/About';
+import Friends from './components/Friends/Friends';
+import NotFound from './components/NotFound/NotFound';
 
 function App() {
   return (
     <div className="App">
-      <Home></Home>
-      <Friends></Friends>
-      <About></About>
+      <h1 className='mb-8 text-2xl mt-2'>Welcome to my fancy routing website!!</h1>
+      <Routes>
+        <Route path='/' element={ <Home></Home>}></Route>
+        <Route path="/about" element={ <About/> } />
+        <Route path="/friends" element={ <Friends/> } />
+        <Route path="*" element={ <NotFound/> } />
+      </Routes>
     </div>
   );
 }
---
+++ Module-52/react-router/src/components/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h2>The page you are looking for is not availabe</h2>
+            <h4 className='text-sm text-red-600 font-bold'>404</h4>
+        </div>
+    );
+};
+
+export default NotFound;
