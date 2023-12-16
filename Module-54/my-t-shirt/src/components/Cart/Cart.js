dfd50f2beb4ea1cd19e13557777adfe97730ebe0
noman-swe <noman.swe130@gmail.com>
2023-12-17 01:46:03 +0600

    54-4 Explore 4 ways to add Conditional Rendering in React

--- Module-54/my-t-shirt/src/components/Cart/Cart.js
+++ Module-54/my-t-shirt/src/components/Cart/Cart.js
@@ -2,6 +2,33 @@ import React from 'react';
 import './Cart.css';
 
 const Cart = ({ cart, handleRemoveFromCart }) => {
+    /* 
+    // conditional rendering options
+        1. element variable
+    */
+
+    //    element variable conditional rendering
+    let command;
+    if (cart.length === 0) {
+        command = <div className='empty-cart-msg'>
+            <h4>Oi Kipta! <br /> khoroch kor...</h4>
+            <p>Kinos na kn??</p>
+        </div>
+    }
+    else if (cart.length === 1) {
+        command = <div className='add-more'>
+            <p >Add more...</p>
+        </div>
+    }
+    else {
+        command = <p className='thanks'>Thanks for adding</p>
+    }
+
+    // conditional formatting operator / ternary operator (condition ? true statement : false statement)
+
+
+
+
 
     return (
         <div className='cart'>
@@ -14,6 +41,17 @@ const Cart = ({ cart, handleRemoveFromCart }) => {
                         <button onClick={() => handleRemoveFromCart(tShirt)}>X</button>
                     </p>)
             }
+            {command}
+
+            {/* ternary operator #conditional Rendering-2 */}
+            {cart.length !== 5 ? <p>Keep adding to 5 products</p> : <button> Remove All</button>}
+
+            {
+                cart.length === 5 && <button className='orange'>Review Order</button>
+            }
+
+            {/* conditional formatting #3 // cart.length === 0 is true or p class is true.*/}
+            {cart.length === 0 || <p className='orange'>yay! let's buy.</p>}
         </div>
     );
 };
