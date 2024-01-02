09a8481528063cb29e84bfdf5b0a1a576b73a405
noman-swe <noman.swe130@gmail.com>
2024-01-02 15:17:56 +0600

    55_5-4 Implement Search With Simple Code

--- milestone-bonus/src/components/Main/Main.js
+++ milestone-bonus/src/components/Main/Main.js
@@ -4,18 +4,26 @@ import Activity from '../Activity/Activity';
 import useVolunteers from '../../hooks/useVolunteers';
 
 const Main = () => {
-    const [volunteers, setVolunteers] = useVolunteers();
+    const [volunteers] = useVolunteers();
+    const [searchResult, setSearchResult] = useState([]);
+
+    const handleSearchChange = event => {
+        const searchText = event.target.value;
+        const matchResult = volunteers.filter(v => v.title.toLowerCase().includes(searchText));
+        setSearchResult(matchResult);
+    }
 
     return (
         <div>
             <h2>Volunteer Activities: {volunteers.length}</h2>
 
-            <div style={{ 'margin': '20px' }}>
-                <input type="text" placeholder='search' />
+            <div style={{ 'margin': '20px', 'border': '1px solid gray', 'padding': '26px 50px 50px 50px', 'width': '400px', 'margin': '0 auto', 'marginBottom': '20px', 'borderRadius': '10px' }}>
+                <p style={{ 'fontSize': '20px' }}>Search for your Volunteer.</p>
+                <input onChange={handleSearchChange} type="text" placeholder='search' style={{ 'padding': '20px', 'fontSize': '20px' }} />
             </div>
             <div className="activity-container">
                 {
-                    volunteers.map(activity => <Activity
+                    searchResult.map(activity => <Activity
                         key={activity._id}
                         activity={activity}
                     ></Activity>)
