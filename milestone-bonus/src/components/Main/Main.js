1d3597acd67cb2a4d66744786fd5e811b57c78ec
noman-swe <noman.swe130@gmail.com>
2024-01-02 15:32:50 +0600

    55_5-5 Use Effect With Dependency List

--- milestone-bonus/src/components/Main/Main.js
+++ milestone-bonus/src/components/Main/Main.js
@@ -4,18 +4,34 @@ import Activity from '../Activity/Activity';
 import useVolunteers from '../../hooks/useVolunteers';
 
 const Main = () => {
-    const [volunteers] = useVolunteers();
+    // const [volunteers] = useVolunteers();
+    const [searchText, setSearchText] = ('');
     const [searchResult, setSearchResult] = useState([]);
 
+    useEffect(() => {
+        console.log('inside the use Effect');
+        fetch('data.json')
+        .then(res => res.json())
+        .then(data =>{
+            const matchResult = data.filter(d => d.title.toLowerCase().includes(searchText));
+            setSearchResult(matchResult);
+        })
+    }, [searchText])
+
     const handleSearchChange = event => {
+        setSearchText(event.target.value);
+
+    }
+    /* 
+    const handleSearchChange2 = event => {
         const searchText = event.target.value;
         const matchResult = volunteers.filter(v => v.title.toLowerCase().includes(searchText));
         setSearchResult(matchResult);
     }
-
+ */
     return (
         <div>
-            <h2>Volunteer Activities: {volunteers.length}</h2>
+            <h2>Volunteer Activities: {searchResult.length}</h2>
 
             <div style={{ 'margin': '20px', 'border': '1px solid gray', 'padding': '26px 50px 50px 50px', 'width': '400px', 'margin': '0 auto', 'marginBottom': '20px', 'borderRadius': '10px' }}>
                 <p style={{ 'fontSize': '20px' }}>Search for your Volunteer.</p>
